ed928fc1f32bb4562068919d4a996ae8224e6d5b
Dev Wibu <tantnse184055@fpt.edu.vn>
2026-01-26 08:46:18 +0700

    feat: Add error handling pages for 401, 403, 404, 500, 503, and 504; update index export

---
+++ src/pages/Error/index.ts
@@ -0,0 +1,6 @@
+export { NotFoundPage } from "./NotFoundPage";
+export { ServerErrorPage } from "./ServerErrorPage";
+export { ServiceUnavailablePage } from "./ServiceUnavailablePage";
+export { ForbiddenPage } from "./ForbiddenPage";
+export { UnauthorizedPage } from "./UnauthorizedPage";
+export { GatewayTimeoutPage } from "./GatewayTimeoutPage";
