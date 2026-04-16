7a9761975ad87f001340283d9c253f9ae7fb677c
Dev Wibu <tantnse184055@fpt.edu.vn>
2026-04-16 08:38:55 +0700

    delete unused file

--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,8 +1,4 @@
 export { AuthLayout } from "./AuthLayout";
 export { Footer } from "./Footer";
 export { Header } from "./Header";
-export { MainLayout } from "./MainLayout";
-export { MentorDashboardLayout } from "./MentorDashboardLayout";
-export { MentorSidebar } from "./MentorSidebar";
-export { Sidebar } from "./Sidebar";
-export { UserDashboardLayout } from "./UserDashboardLayout";
+
