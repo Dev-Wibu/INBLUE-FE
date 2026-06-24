29cf823a420ddff6912516bd16469ec7a1c3c2a6
TuanNguyen212204 <tuan51463@gmail.com>
2026-06-25 01:19:13 +0700

    chore: remove temp script files from root directory

--- fix-commit.py
+++
@@ -1,8 +0,0 @@
-# Use git filter-repo to remove co-authored-by
-git filter-repo --force --message-callback '
-import re
-if re.search(r"^Co-authored-by:.*$", message, re.MULTILINE):
-    message = re.sub(r"\nCo-authored-by:.*$", "", message, flags=re.MULTILINE)
-    changed = True
-return message
-'
