65ab7dd124de740c73662d4dc1cf88aa54237e77
Dev Wibu <tantnse184055@fpt.edu.vn>
2026-07-29 14:47:06 +0700

    refactor(auth): remove unused WaitingAcceptMentorPage zombie component

--- src/pages/Auth/index.ts
+++ src/pages/Auth/index.ts
@@ -3,4 +3,3 @@ export { ForgotPasswordShell } from "./ForgotPasswordShell";
 export { LoginPage } from "./LoginPage";
 export { ResetPasswordPage } from "./ResetPasswordPage";
 export { SignupPage } from "./SignupPage";
-export { WaitingAcceptMentorPage } from "./WaitingAcceptMentorPage";
